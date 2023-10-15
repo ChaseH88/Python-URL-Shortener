51c2ffcad0895c3079f8f2bace3011ceac843adc
Chase Harrison <chaseharrison@Chases-Laptop.attlocal.net>
2023-10-15 17:59:59 -0500

    finish url shortener

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 import random
 import string
-
 
 app = Flask(__name__)
 
@@ -16,13 +15,16 @@ def generate_random_string(length=5):
 def main():
     url = request.args.get('url', default=None, type=str)
 
+    if len(url) == 5:
+      for key, value in key_value_store.items():
+        if value == url:
+          print("Redirecting to: ", key)
+          return redirect(key)
+
     if url is not None and url not in key_value_store:
         key_value_store[url] = generate_random_string(5)
 
-    return {
-        "url": url,
-        "key_value_store": key_value_store
-    }
+    return key_value_store[url]
 
 if __name__ == "__main__":
     app.run(debug=True)
