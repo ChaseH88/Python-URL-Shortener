96f31cc8f0e62d80cb2927a5dc512f328ad9b150
Chase Harrison <chaseharrison@Chases-Laptop.attlocal.net>
2023-10-15 15:13:24 -0500

    setup query param

--- api.py
+++ api.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
+    key = request.args.get('key', default=None, type=str)
+
     return {
-        "message": "testing"
+        "key": key
     }
+
+if __name__ == "__main__":
+    app.run(debug=True)
