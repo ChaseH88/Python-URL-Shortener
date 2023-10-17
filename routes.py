1269a98050ec7c41e76d0fb2ac67da215d044735
Chase Harrison <ChaseH88@gmail.com>
2023-10-17 07:12:47 -0500

    refactor app to use Store class

--- routes.py
+++ routes.py
@@ -1,23 +1,25 @@
 from flask import request, redirect
 from utils import generate_random_string
-from store import key_value_store
+from store import store
 
 def main():
     url = request.args.get('url', default=None, type=str)
 
     if url is not None:
-        if url not in key_value_store and len(url) > 0:
-            key_value_store[url] = generate_random_string(5)
-            return key_value_store[url]
-        elif url in key_value_store:
-            return key_value_store[url]
+        if store.getKey(url) is None and len(url) > 0:
+            key = generate_random_string(5)
+            store.add(url, key)
+            return key
+        elif store.getKey(url) is not None:
+            return store.getKey(url)
         else:
             return "Invalid URL"
     else:
         return "No URL provided"
-    
+
 def redirect_to_url(key):
-    for original_url, stored_key in key_value_store.items():
+    store_data = store.getStore()
+    for original_url, stored_key in store_data.items():
         if stored_key == key:
             return redirect(original_url)
     return "Invalid URL"
--- store.py
+++ store.py
@@ -1,1 +1,27 @@
 key_value_store = {}
+
+class Store:
+    def __init__(self):
+        self.key_value_store = {}
+
+    def add(self, key, value):
+        if key not in self.key_value_store and len(key) > 0:
+            self.key_value_store[key] = value
+
+    def getKey(self, key):
+        return self.key_value_store.get(key, None)
+    
+    def getValue(self, value):
+        for k, v in self.key_value_store.items():
+            if v == value:
+                return k
+        return None
+    
+    def getStore(self):
+        return self.key_value_store
+    
+    def delete(self, key):
+        if key in self.key_value_store:
+            del self.key_value_store[key]
+
+store = Store()
