1265dc55c0eb8c81236529d115da0ccd3d0cfe8c
Chase Harrison <ChaseH88@gmail.com>
2023-10-17 07:30:48 -0500

    rename routes

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from routes import main, redirect_to_url, serve_ui
 
 app = Flask(__name__)
 
-app.add_url_rule('/', 'main', main)
+app.add_url_rule('/', 'main', serve_ui)
 app.add_url_rule('/<key>', 'redirect_to_url', redirect_to_url)
-app.add_url_rule('/ui', 'serve_ui', serve_ui)
+app.add_url_rule('/ui', 'serve_ui', main)
 
 if __name__ == "__main__":
     app.run(debug=True)
